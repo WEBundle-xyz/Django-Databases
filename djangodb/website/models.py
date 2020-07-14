97f4da7c21337ce0585e9e8579eecbc092b13191
Stefan Griffin <griffinstef39@gmail.com>
2020-07-13 23:32:23 -0400

    Added one table named Members

--- djangodb/website/models.py
+++ djangodb/website/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from django.db import models 
 
-# Create your models here.
+class Members(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    passwd = models.CharField(max_length=50)
+    age = models.CharField(max_length=3)
+
+
+    def __str__(self):
+        return self.fname + ' ' + self.lname
+
