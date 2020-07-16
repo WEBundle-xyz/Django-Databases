4b024281ca5c784797c5274643867c2f72e401ac
Stefan Griffin <griffinstef39@gmail.com>
2020-07-15 21:44:59 -0400

    Creating web forms

--- djangodb/website/views.py
+++ djangodb/website/views.py
@@ -5,6 +5,7 @@ def home(request):
     all_members = Members.objects.all
     return render(request, 'home.html', {'all': all_members})
 
-
+def join(request):
+    return render(request, 'join.html', {})
 
 
